79223c2515888a252295a473b6cecb20430c1ab1
edo <edoardo.branchi@stud.unifi.it>
2020-10-27 14:33:59 +0100

    aggiunte CDN per il plugin js

--- grafici.php
+++ grafici.php
@@ -34,7 +34,7 @@
 
         <!-- Importa i file dei singoli grafici-->
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="/js/chart.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
         <script type="text/javascript" src="/js/app.js"></script>
         <script type="text/javascript" src="/js/app2.js"></script>
         <script type="text/javascript" src="/js/app3.js"></script>
